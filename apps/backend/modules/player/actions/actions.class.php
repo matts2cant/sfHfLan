257938eff41811da8f89735f09bc49e1f6e32b71
matts2cant <matts2cant@gmail.com>
2012-02-26 00:37:18 +0100

    [backend] Booking enabled in player module

--- apps/backend/modules/player/actions/actions.class.php
+++ apps/backend/modules/player/actions/actions.class.php
@@ -15,7 +15,30 @@ class playerActions extends autoPlayerActions
 {
   public function executeListBooking(sfWebRequest $request)
   {
+    $this->form = new BookingForm();
 
+    if ($request->isMethod('post'))
+    {
+      $this->form->bind($request->getPostParameters());
+      if ($this->form->isValid())
+      {
+        $values = $this->form->getValues();
+        $emails = $values['input'];
+        $tournament = TournamentTable::getInstance()->findOneById($values['tournament']);
+
+        foreach($emails as $email => $nick)
+        {
+          $player = $this->createBookedPlayer($email, $nick, $tournament);
+          sendConfirmationMail($player);
+        }
+
+        $this->getUser()->setFlash('notice', count($emails)." reservations were made.");
+      }
+      else
+      {
+        $this->getUser()->setFlash('error', "Erreurs dans le formulaire.");
+      }
+    }
   }
 
   public function executeListMailingList(sfWebRequest $request)
@@ -38,6 +61,19 @@ class playerActions extends autoPlayerActions
 
   }
 
+  protected function createBookedPlayer($email, $nick, $tournament)
+  {
+    $player = new Player();
+    $player->setFirstname("-");
+    $player->setLastname("-");
+    $player->setNickname($nick);
+    $player->setEmail($email);
+    $player->setTournament($tournament);
+    $player->save();
+
+    return $player;
+  }
+
   protected function getPlayersFromFilter()
   {
     $holder = $this->getUser()->getAttributeHolder()->getAll('admin_module');
@@ -57,5 +93,29 @@ class playerActions extends autoPlayerActions
     }
 
     return $query->execute();
+  }
+
+  protected function sendConfirmationMail($player)
+  {
+    $editUrl = $this->getController()->genUrl(array('sf_route' => "registration_edit_player", 'sf_subject' => $player), false);
+    $cancelUrl = $this->getController()->genUrl(array('sf_route' => "registration_cancel_player", 'sf_subject' => $player), false);
+
+    $message = $this->getMailer()->compose(
+      array('noreply@hf-lan.fr' => 'hf.lan'),
+      $player->getEmail(),
+      'Confirmation de votre pré-inscription à la hf-lan.',
+      <<<EOF
+Vous avez été pré-inscrit à le hf-lan !
+
+Pour finaliser votre inscription, merci de compléter votre fiche à l'adresse suivante :
+$editUrl
+
+Si vous souhaitez finalement vous désinscrire, cliquez sur le lien suivant :
+$cancelUrl
+
+Pour toute information complémentaire, n'hésitez pas à contecter le staff hf.lan.
+EOF
+    );
+    $this->getMailer()->send($message);
   }
 }
--- apps/backend/modules/player/templates/listBookingSuccess.php
+++ apps/backend/modules/player/templates/listBookingSuccess.php
@@ -1,1 +1,13 @@
 <h1>Gestion des réservations</h1>
+
+<form method="post" action="<?php echo url_for("player/listBooking"); ?>">
+  <table class="booking">
+    <?php echo $form; ?>
+  </table>
+  <tr>
+    <td></td>
+    <td>
+      <input type="submit" />
+    </td>
+  </tr>
+</form>
