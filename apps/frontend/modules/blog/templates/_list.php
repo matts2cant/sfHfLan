f2c4b51f5176cd425c48db0c2b477f99098f52a4
matts2cant <matts2cant@gmail.com>
2012-02-25 02:32:55 +0100

    [frontend] Changes to to blog list partial

--- apps/frontend/modules/blog/templates/_list.php
+++ apps/frontend/modules/blog/templates/_list.php
@@ -1,14 +1,15 @@
+<?php use_stylesheet('blog.css') ?>
 <?php use_helper('Text') ?>
 <?php use_helper('Escaping') ?>
 
 <?php foreach($blogArticles as $article):?>
 <div class="news">
-    <h2>
+    <h3>
         <?php echo $article->getTitle();?>
         <span class="small">
             [ <?php echo $article->getDateTimeObject('created_at')->format('d/m/Y');?> ]
         </span>
-    </h2>
+    </h3>
     <br/>
     <p><?php echo truncate_text(strip_tags($article->getContent(ESC_RAW)), 300, "...");?></p>
     <a class="more" href="<?php echo url_for('blog_view', $article); ?>">Lire la suite &raquo;</a>
