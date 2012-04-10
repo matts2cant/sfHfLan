24730c6e736888aef9246a2230460863deb45833
matts2cant <matts2cant@gmail.com>
2012-04-10 21:52:33 +0200

    [master] Added twitter bootstrap

---
+++ lib/task/sfTwitterBootstrapCompileLessTask.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class sfTwitterBootstrapCompileLessTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_OPTIONAL, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_OPTIONAL, 'The connection name', 'doctrine'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'tb';
+    $this->name             = 'compile-less';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [sfTwitterBootstrapCompileLess|INFO] task does things.
+Call it with:
+
+  [php symfony sfTwitterBootstrapCompileLess|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    system("lessc www/less/bootstrap.less > www/css/bootstrap.css");
+  }
+}
